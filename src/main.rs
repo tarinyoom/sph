6c2b09e26e0a302325ccffaccaed28aed50561f2
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2023-11-19 22:56:14 +0000

    adds people

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,33 @@ use bevy::prelude::*;
 
 const GREETING: &str = "hello world!";
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
 fn hello_world() {
     println!("{}", GREETING);
 }
 
 fn main() {
-    App::new().add_systems(Startup, hello_world).run();
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+                     .run();
 }
 
 #[cfg(test)]
