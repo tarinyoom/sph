2be7d428c4a7a83b40d5bdbde8330e92687422ca
Adam <adam@tarinyoom.io>
2024-02-18 01:52:15 -0800

    creates fluid physical parameter resource

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,41 @@ mod fluid;
 mod types;
 
 use fluid::update_particle;
-use types::{Bounds, Particle};
+use types::{Bounds, FluidParams, Particle};
 
 use rand::prelude::*;
 use std::f32::consts::PI;
 
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
-const NUM_PARTICLES: i32 = 1000;
-const RADIUS: f32 = 1.0;
-const SPEED: f32 = 25.0;
-const MIN_BOUNDS: [f32; 2] = [-500.0, -300.0];
-const MAX_BOUNDS: [f32; 2] = [500.0, 300.0];
-const BOUNDS: Bounds = Bounds {
-    min: Vec2::new(MIN_BOUNDS[0] + RADIUS, MIN_BOUNDS[1] + RADIUS),
-    max: Vec2::new(MAX_BOUNDS[0] - RADIUS, MAX_BOUNDS[1] - RADIUS),
+const DEFAULT_FLUID_PARAMS: FluidParams = FluidParams {
+    n: 1000,
+    speed: 25.0,
+    bounds: Bounds {
+        min: Vec2::new(-500.0, -300.0),
+        max: Vec2::new(500.0, 300.0),
+    },
 };
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(DEFAULT_FLUID_PARAMS)
         .add_systems(Startup, setup)
         .add_systems(Update, update_particles)
         .run();
 }
 
-fn random_particle() -> Particle {
+fn random_particle(fluid: &Res<FluidParams>) -> Particle {
     let mut rng = rand::thread_rng();
 
     let mut x = Vec2::ZERO;
     for i in 0..2 {
-        x[i] = (rng.gen::<f32>() - 0.5) * (MAX_BOUNDS[i] - MIN_BOUNDS[i] - 2.0 * RADIUS);
+        x[i] = (rng.gen::<f32>() - 0.5) * (fluid.bounds.max[i] - fluid.bounds.min[i]);
     }
 
     let a = rng.gen::<f32>() * 2.0 * PI;
-    let v = Vec2::from_angle(a) * SPEED;
+    let v = Vec2::from_angle(a) * fluid.speed;
 
     Particle {
         position: x,
@@ -48,25 +48,30 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    fluid: Res<FluidParams>,
 ) {
-    for _ in 0..NUM_PARTICLES {
+    for _ in 0..fluid.n {
         commands.spawn((
             MaterialMesh2dBundle {
-                mesh: meshes.add(shape::Circle::new(1.).into()).into(),
+                mesh: meshes.add(shape::Circle::new(2.).into()).into(),
                 material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
                 transform: Transform::from_translation(Vec3::ZERO),
                 ..default()
             },
-            random_particle(),
+            random_particle(&fluid),
         ));
     }
 
     commands.spawn(Camera2dBundle::default());
 }
 
-fn update_particles(mut cubes: Query<(&mut Transform, &mut Particle)>, timer: Res<Time>) {
+fn update_particles(
+    mut cubes: Query<(&mut Transform, &mut Particle)>,
+    timer: Res<Time>,
+    fluid: Res<FluidParams>,
+) {
     for (mut transform, mut p) in &mut cubes {
-        *p = update_particle(&p, &BOUNDS, timer.delta_seconds());
+        *p = update_particle(&p, &fluid.bounds, timer.delta_seconds());
         transform.translation = p.position.extend(0.0);
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,16 @@
-use bevy::prelude::{Component, Vec2};
+use bevy::prelude::{Component, Resource, Vec2};
 
 #[derive(Component, Debug, PartialEq)]
 pub struct Particle {
     pub position: Vec2,
     pub velocity: Vec2,
+}
+
+#[derive(Resource)]
+pub struct FluidParams {
+    pub n: i32,
+    pub speed: f32,
+    pub bounds: Bounds,
 }
 
 pub struct Bounds {
